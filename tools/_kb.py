0943f4c16a336586116290ab7bc185e1da177728
Jak Crow <jak@jxcrw.io>
2023-06-22 01:46:52 -0700

    +kbd: Practice

--- tools/_kb.py
+++ tools/_kb.py
@@ -843,7 +843,7 @@ class Solution38_1:
 
 # 39: 2023/06/21
 class Solution39:
-    def maxProfit(self, prices: list[int]) -> int:
+    def max_profit(self, prices: list[int]) -> int:
         price_min, profit_max = float('inf'), 0
         for price in prices:
             price_min = min(price_min, price)
@@ -853,7 +853,7 @@ class Solution39:
 
 
 class Solution39_1:
-    def numIslands(self, grid: list[list[str]]) -> int:
+    def num_islands(self, grid: list[list[str]]) -> int:
         count, m, n = 0, len(grid), len(grid[0])
         for i in range(m):
             for j in range(n):
@@ -873,3 +873,45 @@ class Solution39_1:
             is_in_bounds = (0 <= i_next < m) and (0 <= j_next < n)
             if is_in_bounds and grid[i_next][j_next] == '1':
                 self._dfs(grid, i_next, j_next)
+
+
+# 40: 2023/06/22
+class Solution40:
+    def num_islands(self, grid: list[list[int]]) -> int:
+        count, m, n = 0, len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self._dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def _dfs(self, grid: list[list[int]], i: int, j: int) -> None:
+        if grid[i][j] == '#': return
+        grid[i][j] = '#'
+
+        m, n = len(grid), len(grid[0])
+        deltas = ((-1, 0), (1, 0), (0, 1), (0, -1))
+        for di, dj in deltas:
+            i_next, j_next = i + di, j + dj
+            is_in_bounds = (0 <= i_next < m) and (j <= j_next < n)
+            if is_in_bounds and grid[i_next][j_next] == '1':
+                self._dfs(grid, i_next, j_next)
+
+
+class Solution40_1:
+    def product_except_self(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        products = [1] * n
+
+        product_left = 1
+        for i in range(n):
+            products[i] *= product_left
+            product_left *= nums[i]
+
+        product_right = 1
+        for i in reversed(range(n)):
+            products[i] *= product_right
+            product_right *= nums[i]
+
+        return products
